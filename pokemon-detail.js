22bb14aa08cb5399016ec03cffee588a442e3d95
kerynntoh <kerynn@ichat.sp.edu.sg>
2025-04-21 12:30:45 +0800

    detail.html done

---
+++ pokemon-detail.js
@@ -0,0 +1,8 @@
+let currentPokemonId = null;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const MAX_POKEMON = 1304;
+    const pokemonID = new URL (window.location.search).get("id");
+    const id = parseInt(pokemonID, 10); //turns it into a number 
+
+});
